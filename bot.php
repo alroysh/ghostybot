a4facdda4c1f97362e2dd5103fb9781eb2e8457c
alroysh <cicak.jatuh02@gmail.com>
2017-05-23 20:00:56 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -28,8 +28,8 @@ function hajar($yuerel, $dataAing = null) {
 
 require_once('./line_class.php');
 
-$channelAccessToken = ''; //sesuaikan 
-$channelSecret = '';//sesuaikan
+$channelAccessToken = 'foHivR9RW1cwM7LwHhSBOPTAjGa8o8kbmomtLhC906UPPWoB1gIsMhCXh7oE9bGA4HcnU1iGygo06OflcHmU827yGqF3qGQtwPReKwcx+QTOKHKqRFcCDFysPvqeHESKUm4Ey4gPabfHkJeT5FzOdQdB04t89/1O/w1cDnyilFU='; //sesuaikan 
+$channelSecret = 'c1ab49d4f21251d9632f634a25605d24 ';//sesuaikan
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
